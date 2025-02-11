54ac96a86b5ff7da8ea54731d655ac9bf24c69e6
김선미 <kimsunmi@gimseonmis-MacBook-Air.local>
2025-02-11 17:47:25 +0900

    Feat: Order Controller, Service, Dto 개발

---
+++ src/main/java/kr/kookmin/jeongo3/Orders/Dto/OrdersMapping.java
@@ -0,0 +1,10 @@
+package kr.kookmin.jeongo3.Orders.Dto;
+
+import kr.kookmin.jeongo3.Item.Item;
+
+public interface OrdersMapping {
+
+    String getId();
+    Item getItem();
+
+}
---
+++ src/main/java/kr/kookmin/jeongo3/Orders/Dto/RequestOrdersDto.java
@@ -0,0 +1,9 @@
+package kr.kookmin.jeongo3.Orders.Dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestOrdersDto {
+
+    private String itemId;
+}
